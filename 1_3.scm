5241cf2c63f44c0ab433684f284850848f9cf6bb
e115763 <e115763@gmail.com>
2012-04-28 15:53:24 +0900

    add fuga in 1_3.scm

--- 1_3.scm
+++ 1_3.scm
@@ -1,6 +1,18 @@
 (define (hoge x y z)
 	 (* (mid x y z) (high x y z)))
 
+(define (fuga x y z)
+  (/ (* x y z) (low x y z)))
+
+(define (low x y z)
+  (if (< x y)
+	(if (< z x)
+		z
+		x)
+	(if (< z y)
+	  z
+	  y)))
+
 (define (mid x y z)
   (if (> x y)
 	(if (> y z)
@@ -21,11 +33,16 @@
 
 
 
+
+
 ;(use slib)
 ;(require 'trace)
 ;(trace mid)
 
 (hoge 134 1222 3)
+(fuga 134 1222 3)
 (hoge 5 2 3)
+(fuga 5 2 3)
 (hoge 1 2 13)
+(fuga 1 2 13)
 	  
