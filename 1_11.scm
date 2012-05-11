2cf34b1028cac218c0a3450007fb695f370c3e00
e115763 <e115763@gmail.com>
2012-05-11 13:28:19 +0900

    complete 1.11

--- 1_11.scm
+++ 1_11.scm
@@ -1,3 +1,6 @@
+(use slib)
+(require 'trace)
+
 ;再帰的プロセス
 (define (f n)
   (if (< n 3)
@@ -6,4 +9,21 @@
 	   (f (- n 2))
 	   (f (- n 3)))))
 
+;(trace f)
+(f 30)
+
+
+
+(define (f n)
+  (define (f-iter count one two three)
+	(if (= count n)
+	  (if (< count 3)
+		n
+		(+ one two three))
+	  (if (< count 3)
+		(f-iter (+ count 1) count one two)
+		(f-iter (+ count 1) (+ one two three) one two))))
+  (f-iter 0 0 0 0))
+
+;(trace f)
 (f 30)
