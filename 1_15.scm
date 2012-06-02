9b0c1ff96a1060e1708e5bfd81dbc5f8457da580
e115763 <e115763@gmail.com>
2012-06-02 14:52:04 +0900

    add 1.15

---
+++ 1_15.scm
@@ -0,0 +1,31 @@
+(define (cube x) (* x x x))
+(define (p x) (- (* 3 x) (* 4 (cube x))))
+(define (sine angle)
+  (if (not (> (abs angle) 0.1))
+	angle
+	(p (sine (/ angle 3.0)))))
+
+(use slib)
+(require 'trace)
+(trace p)
+(sine 12.15)
+
+;a : (sine 12.15) で手続きpは何回作用させられたか
+;pはangleの絶対値が0.1以下になるまで繰り返すので
+; 1 : 4.05
+; 2 : 1.35
+; 3 : 0.45
+; 4 : 0.15
+; 5 : 0.05
+;ので5回
+;sineではsineを2回呼ぶので、高さが5の二分木になるので2^5で32回
+
+;b : (sine a)の計算量は？
+;2^nくらい？
+;sineはsineを2回呼ぶので。2^nくらい
+;pは(a / (** 3 k))くらいの回数呼ばれる
+;pを呼ぶ回数は少ないけど、一応nに比例してる、と思う
+;sine = 2sine
+;2sine = 2*2sine
+;2*2sine = 2*2*2sine
+;ってなるので。繰り返しの回数は(a/(** 3 k))回くらい。
