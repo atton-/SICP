06fa1a2546beccab94bf286b08fef0266a8fd753
e115763 <e115763@gmail.com>
2012-07-07 14:43:43 +0900

    add my-expr in 1.16

--- 1_16.scm
+++ 1_16.scm
@@ -8,3 +8,23 @@
 		(else (* b (fast-expr b (- n 1))))))
 
 (fast-expr 2 10)
+
+(define (my-expr-iter a b n)
+  (cond ((= n 0) a)
+		((even? n) (my-expr-iter a (square b) (/ n 2)))
+		(else (my-expr-iter (* a b) b (- n 1)))))
+
+(define (my-expr b n)
+  (my-expr-iter 1 b n))
+
+(my-expr 2 10)
+
+(fast-expr 2 100)
+(my-expr 2 100)
+
+(fast-expr 2 1000)
+(my-expr 2 1000)
+
+;my-expr
+;nが奇数の時 ab b^(n-1) にする
+;nが偶数の時 a (b^2)^(n/2) にする
