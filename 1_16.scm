12042eee391815f807f0d241823fac0f59e86bdc
e115763 <e115763@gmail.com>
2012-08-19 16:57:31 +0900

    fix typo 1.16

--- 1_16.scm
+++ 1_16.scm
@@ -2,29 +2,29 @@
   (= (remainder n 2) 0))
 (define (square n) (* n n))
 
-(define (fast-expr b n)
+(define (fast-expt b n)
   (cond ((= n 0) 1)
-		((even? n) (square (fast-expr b (/ n 2))))
-		(else (* b (fast-expr b (- n 1))))))
+		((even? n) (square (fast-expt b (/ n 2))))
+		(else (* b (fast-expt b (- n 1))))))
 
-(fast-expr 2 10)
+(fast-expt 2 10)
 
-(define (my-expr-iter a b n)
+(define (my-expt-iter a b n)
   (cond ((= n 0) a)
-		((even? n) (my-expr-iter a (square b) (/ n 2)))
-		(else (my-expr-iter (* a b) b (- n 1)))))
+		((even? n) (my-expt-iter a (square b) (/ n 2)))
+		(else (my-expt-iter (* a b) b (- n 1)))))
 
-(define (my-expr b n)
-  (my-expr-iter 1 b n))
+(define (my-expt b n)
+  (my-expt-iter 1 b n))
 
-(my-expr 2 10)
+(my-expt 2 10)
 
-(fast-expr 2 100)
-(my-expr 2 100)
+(fast-expt 2 100)
+(my-expt 2 100)
 
-(fast-expr 2 1000)
-(my-expr 2 1000)
+(fast-expt 2 1000)
+(my-expt 2 1000)
 
-;my-expr
+;my-expt
 ;nが奇数の時 ab b^(n-1) にする
 ;nが偶数の時 a (b^2)^(n/2) にする
